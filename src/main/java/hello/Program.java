ea2c7cb27d099c6107b48e150bc5749591217936
cmusman2 <cmusman@hotmail.com>
2017-04-18 20:09:15 +0100

    changes

--- src/main/java/hello/Program.java
+++ src/main/java/hello/Program.java
@@ -1,12 +1,12 @@
 package hello;
 
 public class Program {
-
+/*
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("World");
 		System.out.println("Now");
     
-	}
+	}*/
 
 }
